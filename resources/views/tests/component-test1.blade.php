2a8fee9c9d98c11c26eb6168091affe824842113
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-01 16:23:01 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,5 @@
   <x-slot name="header">ヘッダー１</x-slot>
   コンポーネントテスト１
 
-  <x-tests.card title="タイトル１" content="本文１"></x-tests.card>
+  <x-tests.card title="タイトル１" content="本文１" :message="$message"></x-tests.card>
 </x-tests.app>
