e2fe9650df0f7fc5eb08cec4362d174bb8eefcc5
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-01 16:56:38 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
   コンポーネントテスト１
 
   <x-tests.card title="タイトル１" content="本文１" :message="$message"></x-tests.card>
-  <x-tests.card title="タイトル２"></x-tests>
+  <x-tests.card title="タイトル２"></x-tests.card>
+  <x-tests.card title="CSSを変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
