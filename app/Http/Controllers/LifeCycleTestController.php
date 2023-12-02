f492994c4cef8db619db0a84313ab18ae127e0f7
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-02 17:22:45 +0900

    サービスコンテナ

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest()
+    {
+        // bindメソッドでサービスコンテナに 'lifeCycleTest' を登録
+        app()->bind('lifeCycleTest', function () {
+            return 'ライフサイクルテスト';
+        });
+
+        // makeメソッドで 'lifeCycleTest' を解決し、変数$testに格納
+        $test = app()->make('lifeCycleTest');
+
+        // サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナを使うパターン
+        app()->bind('sample',Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+        // 結果を出力
+        dd($test, app());
+    }
+}
+class Sample
+{
+    public $message;
+    public function __construct(Message $message){
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send(){
+        echo('メッセージを表示');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,7 @@ Route::get('/dashboard', function () {
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
