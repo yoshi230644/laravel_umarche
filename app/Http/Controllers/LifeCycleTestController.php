6aed6ddff0a5273c153a5c4b4dc633bedd7d0a90
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-02 17:52:36 +0900

    サービスプロバイダのテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    //
+    public function showServiceProviderTest()
+    {
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample,$password, $encrypt->decrypt($password));
+    }
+
+
     public function showServiceContainerTest()
     {
         // bindメソッドでサービスコンテナに 'lifeCycleTest' を登録
