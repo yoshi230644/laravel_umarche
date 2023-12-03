e65b51dfe5ed7cf85031078cc95e4121d849e953
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-03 16:35:52 +0900

    リソースコントローラ

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -5,15 +5,20 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
+                    <div class="w-12">
                     <a href="{{ route('admin.dashboard') }}">
                         <x-application-logo class="block h-9 w-28 fill-current text-gray-800 dark:text-gray-200" />
                     </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
+                        オーナー管理
                     </x-nav-link>
                 </div>
             </div>
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -26,6 +27,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners', OwnersController::class)
+->middleware('auth:admin');
+
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
