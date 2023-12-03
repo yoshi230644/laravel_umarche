581ada01c68631334ec882f0e3b965da8168e88a
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-03 19:50:09 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Owner; //Eloquent エロクアント
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB; //QueryBuilder
+use Illuminate\Support\Facades\Hash;
 
 
 
@@ -61,7 +62,22 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:owners'],
+                'password' => ['required', 'confirmed','min:8'],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message','オーナー登録を実施しました。');
     }
 
     /**
