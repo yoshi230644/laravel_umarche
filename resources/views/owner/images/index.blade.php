91b3e7b97914da3ce069dfe5ac1f4db03217086c
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-07 07:02:39 +0900

    edit, update

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -15,12 +15,13 @@
                 </div>
                   <div class="flex flex-wrap">
                     @foreach ($images as $image )
-                      <div class="w-1/4 p-4">
+                      <div class="w-1/4 p-2 md:p-4">
                       <a href="{{ route('owner.images.edit', ['image' => $image->id ])}}">  
-                      <div class="border rounded-md p-4">
+                      <div class="border rounded-md p-2 md:p-4">
                        
-                        <div class="text-xl">{{ $image->title }}</div>
+                        
                         <x-thumbnail :filename="$image->filename" type="products"></x-thumbnail>
+                        <div class="text-gray-700">{{ $image->title }}</div>
                       </div>
                       </a>
                       </div>
