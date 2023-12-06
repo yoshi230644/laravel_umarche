78c4d334fafcc4e0e4115fe14d6d0788783a3ead
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-07 01:52:11 +0900

    shopCreateDelete

--- database/migrations/2023_12_04_114253_create_shops_table.php
+++ database/migrations/2023_12_04_114253_create_shops_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->foreignID('owner_id')->constrained();
+            $table->foreignID('owner_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
             $table->string('name');
             $table->text('information');
             $table->string('filename');
