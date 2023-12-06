06a72c57db16499e6c7854d3e6c69500d8cd8ede
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2023-12-06 09:59:52 +0900

    Shop追加

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('shops')->insert([
+            [
+                'owner_id' => 1,
+                'name' => 'ここに店名が入ります',
+                'information' =>'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => '',
+                'is_selling' => true
+            ],
+            [
+                'owner_id' => 2,
+                'name' => 'ここに店名が入ります',
+                'information' =>'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => '',
+                'is_selling' => true
+            ],
+        ]);
+    }
+}
