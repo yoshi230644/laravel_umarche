c13540dc88f090784b1b976f0dc6e7d44aaa437d
yoshi230644 <142764611+yoshi230644@users.noreply.github.com>
2023-12-14 08:27:55 +0900

    swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import './swiper'; 
+
 window.Alpine = Alpine;
 
 Alpine.start();
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/swiper.js',
+                'public/js',
+                'public/css'
             ],
             refresh: true,
         }),
