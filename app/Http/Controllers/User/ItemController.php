06b85307de7f98c90c2d8690166009e6aaf1c90c
yoshi230644 <142764611+yoshi230644@users.noreply.github.com>
2023-12-14 09:25:58 +0900

    商品詳細の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -49,6 +50,13 @@ class ItemController extends Controller
 
     public function show($id){
         $product = Product::findOrFail($id);
-        return view('user.show', compact('product'));
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
+
+        if($quantity > 9){
+            $quantity = 9;
+            }
+
+        return view('user.show', compact('product','quantity'));
         }
 }
