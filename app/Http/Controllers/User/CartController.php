0a3e35003f5b00c21dd48479f4b610d0e1300808
yoshi230644 <142764611+yoshi230644@users.noreply.github.com>
2024-01-18 09:33:18 +0900

    cartService作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use PHPUnit\TextUI\Configuration\Constant;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
@@ -59,6 +60,10 @@ class CartController extends Controller
 
     public function checkout()
     {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $product=CartService::getItemsInCart($items);
+        ////
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
  
